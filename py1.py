b379067a03176c2135fd400542f5ec17b2261542
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2021-02-08 11:16:57 +0530

    How to accesss a bluetooth module using python

---
+++ py1.py
@@ -0,0 +1,20 @@
+import serial
+import time
+Arduino = serial.Serial('COM1',9600)
+print(Arduino.readline())
+
+print("Message from Arduino ")
+
+while 1:
+    var =  raw_input()
+    if(var > 0):
+        Arduino.write('1')
+        print("Recieved transmission")
+        time.sleep(1)
+    if(var == 0):
+        Arduino.write('0')
+        print("LED turned OFF")
+        time.sleep(1)
+    if(var == "im fine and you"):
+        print("im fine too")
+    
