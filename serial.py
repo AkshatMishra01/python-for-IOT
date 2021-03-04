54e289df7d26b27f492545082a7dfd2a6ae271e0
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2021-03-04 08:04:06 +0530

    Update serial.py

--- serial.py
+++ serial.py
@@ -8,5 +8,5 @@ except s.SerialException:
 ser = s.serial_for_url('', timeout = 1)
 sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
 class s.Serial:
-    def __init__(port='COM1', baudrate=9600, bytesize=EIGHTBITS, parity=PARITY_NONE, stopbits=STOPBITS_ONE, timeout=None,rtscts=False):
+    def __init__(port='COM1', baudrate=9600, bytesize=EIGHTBITS, parity=PARITY_NONE, stopbits=STOPBITS_ONE, timeout=None,rtscts=False) :
         
